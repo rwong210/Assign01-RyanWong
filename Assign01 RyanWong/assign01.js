3a79940678940a3a2f7a43f43eddb0afa07dcf7f
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-01 23:35:53 -0700

    finished as much as possible

--- Assign01 RyanWong/assign01.js
+++ Assign01 RyanWong/assign01.js
@@ -7,16 +7,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // listen for the load and display gallery list
     window.addEventListener('load', (e) => {
+        const loader = document.querySelector("#loader");
+        loader.style.display = "none";
         fetch(galleryAPI)
             .then(response => response.json())
             .then(data => {
                 populateList(data);
-                // hideLoading();
+                hideLoader();
                 getClicked(data);
                 toggleList();
             })
             .catch(error => console.error(error));
-        // showLoading();
+        showLoader();
     });
 
     // populate the Gallery List nav with items from the gallery API
@@ -91,13 +93,13 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch(queryString)
             .then(response => response.json())
             .then(data => {
+                hideLoader();
                 displayPaintings(data);
                 paintView(data);
 
             })
             .catch(error => console.error(error));
-
-
+            showLoader();
         //for (image of images) {
         //    let img = document.createElement("img");
         //   img.setAttribute("src", `${imageURL}${image.filename}`);
@@ -198,7 +200,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let images = document.querySelectorAll(".clickable");
         for (i of images) {
             i.addEventListener('click', (e) => {
-                
+
                 let selectedImg = e.target;
                 let lookup = e.target.alt;
                 let found = gallery.find(g => g.Title === lookup);
@@ -207,14 +209,138 @@ document.addEventListener("DOMContentLoaded", function () {
                 const main = document.querySelector("#main");
                 main.style.display = "none";
                 const paintView = document.querySelector("#paintView");
-                paintView.style.display = "flex";
+                paintView.setAttribute("class", "showLarge")
+                const gBtn = document.querySelector("#listButton");
+                gBtn.style.display = "none";
 
                 let h1 = document.createElement("h1");
                 h1.innerText = found.Title;
-            });
 
+                let h2 = document.createElement("h2");
+                if (found.FirstName != null) {
+                    h2.innerText = `${found.FirstName} ${found.LastName}`;
+                } else {
+                    h2.innerText = `${found.LastName}`;
+                }
+
+
+                let fileLocation = "https://res.cloudinary.com/funwebdev/image/upload/SIZE/art/paintings/square/FILENAME";
+                fileLocation = fileLocation.replace(/SIZE/i, "w_500");
+                fileLocation = fileLocation.replace("FILENAME", `${found.ImageFileName}`);
+                let img = document.createElement("img");
+                img.setAttribute("src", fileLocation);
+
+                let p1 = document.createElement("p");
+                p1.innerText = `Year: ${found.YearOfWork}`;
+                let p2 = document.createElement("p");
+                p2.innerText = `Medium: ${found.Medium}`;
+                let p3 = document.createElement("p");
+                p3.innerText = `Width: ${found.width}`;
+                let p4 = document.createElement("p");
+                p4.innerText = `Height: ${found.Height}`;
+                let p5 = document.createElement("p");
+                p5.innerText = `Copyright: ${found.CopyrightText}`;
+                let p6 = document.createElement("p");
+                p6.innerText = `Gallery Name: ${found.GalleryName}`;
+                let p7 = document.createElement("p");
+                p7.innerText = `City: ${found.GalleryCity}`;
+                let p8 = document.createElement("a");
+                p8.setAttribute("href", found.MuseumLink);
+                p8.innerText = found.MuseumLink;
+                let p9 = document.createElement("p");
+                p9.innerText = `Description:
+
+                ${found.Description}`;
+                let p10 = document.createElement("p");
+
+                // create divs for grid layout
+                let div1 = document.createElement("div");
+                div1.id = "imageContainer";
+                let div2 = document.createElement("div");
+                div2.id = "headerContainer";
+                let div3 = document.createElement("div");
+                div3.id = "detailsContainer";
+                let div4 = document.createElement("div");
+                div4.id = "colorsContainer";
+                let div5 = document.createElement("div");
+                div5.id = "buttonContainer";
+
+                // append painting info to paintView
+                div1.appendChild(img);
+
+                div2.appendChild(h1);
+                div2.appendChild(h2);
+
+                div3.appendChild(p1);
+                div3.appendChild(p2);
+                div3.appendChild(p3);
+                div3.appendChild(p4);
+                div3.appendChild(p5);
+                div3.appendChild(p6);
+                div3.appendChild(p7);
+                div3.appendChild(p8);
+                div3.appendChild(p9);
+
+                // create color spans and append to div4
+                const colors = [...found.JsonAnnotations.dominantColors];
+                for (c of colors) {
+                    let red = Number(c.color.red);
+                    let green = Number(c.color.green);
+                    let blue = Number(c.color.blue);
+                    let cWeb = c.web;
+                    let span = document.createElement("span");
+                    span.innerText = " ";
+                    span.style.backgroundColor = c.web;
+                    div4.appendChild(span);
+                }
+
+                // create button element to close largePaint view
+                let closeBtn = document.createElement("button");
+                closeBtn.id = "closeBtn";
+                closeBtn.innerText = "Close Button";
+                div5.appendChild(closeBtn);
+
+                closeBtn.addEventListener('click', (e) => {
+                    paintView.classList.remove("showLarge");
+                    paintView.innerHTML = " ";
+                    main.style.display = "flex";
+                    gBtn.style.display = "flex";
+
+                })
+
+                // append divs to paintView
+                paintView.appendChild(div1);
+                paintView.appendChild(div2);
+                paintView.appendChild(div3);
+                paintView.appendChild(div4);
+                paintView.appendChild(div5);
+
+                // event for large image click
+                document.querySelector("#imageContainer img").addEventListener('click', (e) => {
+                    div2.style.display = "none";
+                    div3.style.display = "none";
+                    div4.style.display = "none";
+                    paintView.setAttribute("class", "centered");
+                })
+            })
         }
     }
+
+
+    function showLoader() {
+        const loader = document.querySelector("#loader");
+        loader.style.display = "flex";
+        const main = document.querySelector("#main");
+        main.style.display = "none";
+    }
+
+    function hideLoader(){
+        const loader = document.querySelector("#loader");
+        loader.style.display ="none";
+        const main = document.querySelector("#main");
+        main.style.display = "flex";
+    }
+
     // function for comparing for sorting
     function compare(a, b) {
         if (a.GalleryName < b.GalleryName) {
